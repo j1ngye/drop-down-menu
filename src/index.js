638dddfd17064792dcd38bf7d61e27c4c8f5e2dc
j1ngye <216147947+j1ngye@users.noreply.github.com>
2025-08-16 15:41:39 +0800

    Create a function for making dropdown

---
+++ src/index.js
@@ -0,0 +1,34 @@
+function createDropdown(buttonText, listItems) {
+  const container = document.querySelector("#container");
+  const dropDownContainer = document.createElement("div");
+  const button = document.createElement("button");
+  const list = document.createElement("ul");
+
+  button.textContent = buttonText;
+  button.classList.add("toggle");
+  list.style.visibility = "hidden";
+  list.classList.add("menu");
+
+  listItems.forEach((listItem) => {
+    const item = document.createElement("li");
+    item.textContent = listItem.title;
+    list.append(item);
+  });
+
+  button.addEventListener("click", () => {
+    list.style.visibility =
+      list.style.visibility === "hidden" ? "visible" : "hidden";
+  });
+
+  dropDownContainer.appendChild(button);
+  dropDownContainer.appendChild(list);
+  container.appendChild(dropDownContainer);
+}
+
+const listitems = [
+  { title: "Reset" },
+  { title: "Sleep" },
+  { title: "Shut down" },
+];
+
+createDropdown("Apple", listitems);
